863dbea2e6442616361396ecd26105f3d5adb06d
felixpinok <felixpinok@gmail.com>
2016-08-09 04:05:50 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,7 @@
 var start = 0;
+var n = 0;
+var s = 0;
+var y = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -6,7 +9,7 @@ function setup() {
 }
 
 function draw() {
-  var inc = map(mouseY,0, height,0. ,5.);
+  var inc = map(mouseY,0, height,0. ,10);
   background(51);
   stroke(255);
   noFill();
@@ -14,14 +17,15 @@ function draw() {
   var xoff = start;
   for (var x = 0; x < width; x++){
     stroke(255);
- 
-    var y = sin(xoff) * height;
+    var n = map(noise(xoff), 0, 1, -5, 5);
+    var s = map(sin(xoff), -1, 1, 0, height);
+    var y = s + n;
     vertex(x, y);
 
     xoff += inc;
   }
   endShape();
 
-start += inc * map(mouseX,0, width,1 , 10);
+start += inc * map(mouseX,0, width,1. , 1.002);
 
 }
